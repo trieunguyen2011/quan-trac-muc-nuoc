ec1b0251d24bf0f84c16aeb4725363b3393c8179
trieunguyen2011 <nntrieu201199@gmail.com>
2021-10-30 00:17:07 +0700

    master

--- js/chart1.js
+++ js/chart1.js
@@ -1,5 +1,6 @@
 var array_distance_chart1 = new Array();
 var array_time_chart1 = new Array();
+var array_time_update = new Array();
 
 AWS.config.update({
     region: "ap-northeast-1",
@@ -33,22 +34,26 @@ docClient.query(params_chart1, function(err, data) {
             const time_stamp_chart1 = new Date(sample_time_data_chart1);
             min_chart1 = time_stamp_chart1.getMinutes();
             hour_chart1 = time_stamp_chart1.getHours();
+
+            const month_name = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+            date = time_stamp_chart1.getDate();
+            month = month_name[time_stamp_chart1.getMonth()];
+            year = time_stamp_chart1.getFullYear();
+
+            full_time_update = hour_chart1 + ":" + min_chart1 + ', ' + date + "/" + month + "/" + year;
             time_chart1 = hour_chart1 + ":" + min_chart1;
             array_time_chart1.push(time_chart1);
             if (array_time_chart1.length > 10) {
                 array_time_chart1.shift();
             }
+            array_time_update.push(full_time_update);
+            if (array_time_update.length > 10) {
+                array_time_update.shift();
+            }
         }
+        display_time = array_time_update[9];
+        document.getElementById("display_time").innerHTML = display_time;
 
-        // Hiển thị ngày
-        sample_time_data_date = JSON.parse(data.Items[0].sample_time);
-        const time_stamp_date = new Date(sample_time_data_date);
-        const month_name = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-        date = time_stamp_date.getDate();
-        month = month_name[time_stamp_date.getMonth()];
-        year = time_stamp_date.getFullYear();
-        full_time = date + "/" + month + "/" + year;
-        document.getElementById("display_time").innerHTML = full_time;
 
 
         // Biểu đồ
@@ -57,7 +62,7 @@ docClient.query(params_chart1, function(err, data) {
 
         const ctx = document.getElementById("chart-1").getContext("2d");
         const myChart = new Chart(ctx, {
-            type: "bar",
+            type: "line",
             data: {
                 labels: x_data_chart1,
                 datasets: [{
