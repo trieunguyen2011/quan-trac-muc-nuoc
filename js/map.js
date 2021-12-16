7e510be90db8ec226eb43cccdb6742ffd4c88493
trieunguyen2011 <nntrieu201199@gmail.com>
2021-12-17 00:23:31 +0700

    master

--- js/map.js
+++ js/map.js
@@ -27,24 +27,24 @@ docClient.query(params_map1, function(err, data) {
     lat_1_data = data.Items[data.Items.length - 1].gps_data.Latitude;
     lng_1_data = data.Items[data.Items.length - 1].gps_data.Longitude;
     if (lat_1_data == 0) {
-        lat_1_data = 9.283603
-        lng_1_data = 105.7234
+        lat_1_data = 9.290664
+        lng_1_data = 105.713546
     }
     // Tọa độ 2
     docClient.query(params_map2, function(err, data) {
         lat_2_data = data.Items[data.Items.length - 1].gps_data.Latitude;
         lng_2_data = data.Items[data.Items.length - 1].gps_data.Longitude;
         if (lat_2_data == 0) {
-            lat_2_data = 9.283539
-            lng_2_data = 105.717714
+            lat_2_data = 9.288121
+            lng_2_data = 105.729293
         }
         //     // Tọa độ 3
         docClient.query(params_map3, function(err, data) {
             lat_3_data = data.Items[data.Items.length - 1].gps_data.Latitude;
             lng_3_data = data.Items[data.Items.length - 1].gps_data.Longitude;
             if (lat_3_data == 0) {
-                lat_3_data = 9.278525
-                lng_3_data = 105.72178
+                lat_3_data = 9.280206
+                lng_3_data = 105.721778
             }
 
             initMap();
@@ -52,8 +52,8 @@ docClient.query(params_map1, function(err, data) {
             function initMap() {
                 // Thiết lập ban đầu
                 var options = {
-                    center: { lat: 9.281294, lng: 105.721824 },
-                    zoom: 16,
+                    center: { lat: 9.286595, lng: 105.721851 },
+                    zoom: 15,
                     controls: true,
                 };
                 var map = new map4d.Map(document.getElementById("map"), options);
@@ -65,7 +65,7 @@ docClient.query(params_map1, function(err, data) {
                 // map.enable3dMode(true)
 
                 // Tạo đối tượng marker từ MarkerOption
-                marker1 = new map4d.Marker({
+                var marker1 = new map4d.Marker({
                     // Form thông tin
                     // title: "Trạm 1",
                     // snippet: "Độ cao hiện tại: 1,5m",
@@ -78,7 +78,7 @@ docClient.query(params_map1, function(err, data) {
                     position: { lat: lat_1_data, lng: lng_1_data },
                 });
 
-                let marker2 = new map4d.Marker({
+                var marker2 = new map4d.Marker({
                     // Form thông tin
                     // title: "Trạm 2",
                     // snippet: "Độ cao hiện tại: 1,5m",
@@ -91,7 +91,7 @@ docClient.query(params_map1, function(err, data) {
                     position: { lat: lat_2_data, lng: lng_2_data },
                 });
 
-                let marker3 = new map4d.Marker({
+                var marker3 = new map4d.Marker({
                     // Form thông tin
                     // title: "Trạm 3",
                     // snippet: "Độ cao hiện tại: 1,5m",
@@ -109,6 +109,10 @@ docClient.query(params_map1, function(err, data) {
                 marker2.setMap(map);
                 marker3.setMap(map);
 
+                //Lắng nge sự kiện click marker
+                // map.addListener("click", function clickMarker() {
+                // }, { marker: true });
+
                 //Update value of marker
                 docClient.query(params_chart1, function(err, data) {
                     for (let i = 0; i < data.Items.length; i++) {
@@ -116,8 +120,8 @@ docClient.query(params_map1, function(err, data) {
                         array_distance_chart1.push(distance_data_chart1);
                     }
                     //Update value of marker
-                    let FinalhMarker1 = array_distance_chart1[array_distance_chart1.length - 1];
-                    marker1.setInfoContents("Độ cao hiện tại: " + FinalhMarker1 + "\n" + "m");
+                    let FinalMarker1 = array_distance_chart1[array_distance_chart1.length - 1];
+                    marker1.setInfoContents("Độ cao hiện tại: " + FinalMarker1 + "m");
                     marker1.setMap(map);
                 })
                 docClient.query(params_chart2, function(err, data) {
@@ -126,8 +130,8 @@ docClient.query(params_map1, function(err, data) {
                         array_distance_chart2.push(distance_data_chart2);
                     }
                     //Update value of marker
-                    let FinalhMarker2 = array_distance_chart2[array_distance_chart2.length - 1];
-                    marker2.setInfoContents("Độ cao hiện tại: " + FinalhMarker2 + "\n" + "m");
+                    let FinalMarker2 = array_distance_chart2[array_distance_chart2.length - 1];
+                    marker2.setInfoContents("Độ cao hiện tại: " + FinalMarker2 + "m");
                     marker2.setMap(map);
                 })
                 docClient.query(params_chart3, function(err, data) {
@@ -136,11 +140,13 @@ docClient.query(params_map1, function(err, data) {
                         array_distance_chart3.push(distance_data_chart3);
                     }
                     //Update value of marker
-                    let FinalhMarker3 = array_distance_chart3[array_distance_chart3.length - 1];
-                    marker3.setInfoContents("Độ cao hiện tại: " + FinalhMarker3 + "\n" + "m");
+                    let FinalMarker3 = array_distance_chart3[array_distance_chart3.length - 1];
+                    marker3.setInfoContents("Độ cao hiện tại: " + FinalMarker3 + "m");
                     marker3.setMap(map);
                 })
             };
+
+
         });
     });
 });
